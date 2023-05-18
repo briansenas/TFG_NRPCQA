81c62f62dab0a7a8742d1f36f6b4a8907d92118d
briansenas <briansena23@hotmail.com>
2023-05-18 19:05:37 +0200

    Update rotation.py Open3d Visualization

--- rotation/rotation.py
+++ rotation/rotation.py
@@ -10,71 +10,94 @@ from PIL import Image
 from torchvision import transforms
 import cv2
 import argparse
+import copy
+import multiprocessing
+import glob
+import functools 
+from tqdm import tqdm 
 
 def generate_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
     return path
 
-# Camera Rotation
-def camera_rotation(path, img_path,frame_path,video_path,frame_index):
-    pcd = o3d.io.read_point_cloud(path)
-    transform = transforms.Resize(224) 
-    if not os.path.exists(img_path+'/'):
-        os.mkdir(img_path+'/')
-    vis = o3d.visualization.Visualizer()
-    vis.create_window(visible=False)
-    vis.add_geometry(pcd)
-    ctrl = vis.get_view_control()
+def camera_rotation(path, img_path, frame_path, video_path, frame_index):
+    # We define the image transformation
+    transform = transforms.Resize((224, 224)) # fix: pass a tuple instead of an integer
+    # Load pointcloud file
+    cloud = o3d.io.read_point_cloud(path)
+
+    fps = 15
+    size = (1920, 1080)
+
+    video = cv2.VideoWriter(os.path.join(video_path, '030.mp4'), cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
+    # Generate rotated images
     tmp = 0
-    interval = 5.82
-    #fps = 30 
-    #size = (405, 224) 
-    #video = cv2.VideoWriter(video_path + '/' + 'video.avi', cv2.VideoWriter_fourcc('I', '4', '2', '0'), fps, size)
-    # interval represent rotation interval per degree
-    # 12 * interval indicates 12 degreee
-    start = time.time()
+    # Define rotation angles
+    vid_rate = 30
+    rot_deg = 360 / vid_rate
+    rot_mat = np.eye(3)
+    vis = o3d.visualization.Visualizer() # fix: move outside of the loop
+    vis.create_window(visible=False)
+    pcd = copy.deepcopy(cloud) # fix: make a copy of the point cloud before rotating
+    for tmp in range(1,4*vid_rate+1):
+        if tmp < vid_rate:
+            rot_mat = np.dot(rot_mat, o3d.geometry.get_rotation_matrix_from_axis_angle([0, np.radians(rot_deg), 0]))
+        elif tmp < 2*vid_rate:
+            rot_mat = np.dot(rot_mat, o3d.geometry.get_rotation_matrix_from_axis_angle([np.radians(rot_deg), 0, 0]))
+        elif tmp < 3*vid_rate:
+            rot_mat = np.dot(rot_mat, o3d.geometry.get_rotation_matrix_from_axis_angle(
+                [np.radians(rot_deg / math.sqrt(2)), np.radians(rot_deg / math.sqrt(2)), 0]))
+        elif tmp < 4*vid_rate:
+            rot_mat = np.dot(rot_mat, o3d.geometry.get_rotation_matrix_from_axis_angle(
+                [np.radians(rot_deg / math.sqrt(2)), -np.radians(rot_deg / math.sqrt(2)), 0]))
 
-    # begin rotation
-    while tmp<120:
-        tmp+=1
-        if tmp<30:
-            ctrl.rotate(12*interval, 0)
-        elif tmp>=30 and tmp<60:
-            ctrl.rotate(0, 12*interval)
-        elif tmp>=60 and tmp<90:
-            ctrl.rotate(12*interval/math.sqrt(2), 12*interval/math.sqrt(2))
-        elif tmp>=90 and tmp<120:
-            ctrl.rotate(12*interval/math.sqrt(2), -12*interval/math.sqrt(2))
-        vis.poll_events()
-        vis.update_renderer()    
+
+        # Rotate pointcloud
+        pcd.rotate(rot_mat)
+
+        # Convert pointcloud to image
+        vis.clear_geometries()
+        vis.add_geometry(pcd) # fix: use the loaded point cloud
+        vis.update_renderer()
         img = vis.capture_screen_float_buffer(True)
-        img = Image.fromarray((np.asarray(img)* 255).astype(np.uint8))
+        img = Image.fromarray((np.asarray(img) * 255).astype(np.uint8))
 
-        # save the fram_index -th of every 30 frames as the 2D input with resolution of about 1920x1061
-        if (tmp-frame_index) % 30 == 0:
-            img.save(img_path + '/'+str(tmp).zfill(3)+'.png')
+        # Save the frame_index-th of every 30 frames as the 2D input with resolution of about 1920x1061
+        if (tmp - frame_index) % vid_rate == 0:
+            img.save(os.path.join(img_path, f"{tmp:03d}.png")) # fix: use f-string formatting instead of str.zfill
 
-        # save resized imgs as frames of the video with resolution of (,224)
+        rot_mat = np.eye(3)
+
+        # Save videos
+        video.write(cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR))
+
+        # Save resized imgs as frames of the video with resolution of (,224)
         img = transform(img)
-        img.save(frame_path + '/'+str(tmp).zfill(3)+'.png')
+        img.save(os.path.join(frame_path, f"{tmp:03d}.png"))
 
-        # save videos
-        #video.write(cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR))
+    video.release()
+    vis.destroy_window() # fix: destroy the visualizer window
 
-    end = time.time()
-    print("time consuming: ",end-start)
-    vis.destroy_window()
-    del ctrl
-    del vis
+def process_object(one_object_path, img_path, frame_path, video_path, frame_index):
+    obj = os.path.basename(one_object_path)
+    camera_rotation(one_object_path, generate_dir(os.path.join(img_path, obj)), generate_dir(os.path.join(frame_path, obj)), generate_dir(os.path.join(video_path, obj)), frame_index)
+
 
 def projection(path, img_path, frame_path, video_path, frame_index):
-    # find all the objects 
-    objs = os.walk(path)  
-    for path,dir_list,file_list in objs:  
-      for obj in file_list:  
-        one_object_path = os.path.join(path, obj)
-        camera_rotation(one_object_path,  generate_dir(os.path.join(img_path,obj)),   generate_dir(os.path.join(frame_path,obj)),  generate_dir(os.path.join(video_path,obj)), frame_index)
+    # find all the .ply objects
+    objs = glob.glob(os.path.join(path, '**/*.ply'), recursive=True)
+    pool = multiprocessing.Pool()
+    pfunc = functools.partial(process_object,img_path=img_path,
+                              frame_path=frame_path, video_path=video_path,
+                              frame_index=frame_index)
+    for result in tqdm(pool.imap(func=pfunc, iterable=objs), total=len(objs)):
+        continue 
+
+    # for obj_path in objs:
+    #     pool.apply_async(process_object, args=(obj_path, img_path, frame_path, video_path, frame_index))
+    # pool.close()
+    # pool.join()
 
 
 
