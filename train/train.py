dcca704c2ec9305a33b2b06aba88f6393371b75d
briansenas <briansena23@hotmail.com>
2023-05-18 19:08:39 +0200

    Numerical Stable Logistic Regression

--- train/train.py
+++ train/train.py
@@ -15,22 +15,23 @@ from data_loader import VideoDataset_NR_image_with_fast_features
 import ResNet_mean_with_fast
 import time
 
-
-
-
-
-
 def logistic_func(X, bayta1, bayta2, bayta3, bayta4):
-    logisticPart = 1 + np.exp(np.negative(np.divide(X - bayta3, np.abs(bayta4))))
+    denominator = np.abs(bayta4) + 1e-5  # to avoid division by zero
+    numerator = np.negative(X - bayta3)
+    exponent = np.clip(np.divide(numerator, denominator), -500, 500)  # to avoid overflow and underflow
+    logisticPart = 1 + np.exp(exponent)
     yhat = bayta2 + np.divide(bayta1 - bayta2, logisticPart)
     return yhat
 
+
 def fit_function(y_label, y_output):
-    beta = [np.max(y_label), np.min(y_label), np.mean(y_output), 0.5]
-    popt, _ = curve_fit(logistic_func, y_output, \
-        y_label, p0=beta, maxfev=100000000)
+    max_val = np.max(y_label)
+    min_val = np.min(y_label)
+    mean_val = np.mean(y_output)
+    range_val = np.max(y_output) - np.min(y_output)
+    beta = [max_val, min_val, mean_val, range_val]
+    popt, _ = curve_fit(logistic_func, y_output, y_label, p0=beta, maxfev=1000000)
     y_output_logistic = logistic_func(y_output, *popt)
-    
     return y_output_logistic
 
 
