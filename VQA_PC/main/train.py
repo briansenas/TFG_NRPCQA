3b15393f09909b7895e15867cd696d1e318268c5
briansenas <briansena23@hotmail.com>
2023-05-27 17:38:49 +0200

    Merge VQA_PC train/test folder

--- VQA_PC/main/train.py
+++ VQA_PC/main/train.py
@@ -13,26 +13,13 @@ from scipy import stats
 from scipy.optimize import curve_fit
 from data_loader import VideoDataset_NR_image_with_fast_features
 import ResNet_mean_with_fast
+import random 
 import time
 
-def logistic_func(X, bayta1, bayta2, bayta3, bayta4):
-    denominator = np.abs(bayta4) + 1e-5  # to avoid division by zero
-    numerator = np.negative(X - bayta3)
-    exponent = np.clip(np.divide(numerator, denominator), -500, 500)  # to avoid overflow and underflow
-    logisticPart = 1 + np.exp(exponent)
-    yhat = bayta2 + np.divide(bayta1 - bayta2, logisticPart)
-    return yhat
+torch.manual_seed(140421)
+random.seed(140421)
+np.random.seed(140421) 
 
-
-def fit_function(y_label, y_output):
-    max_val = np.max(y_label)
-    min_val = np.min(y_label)
-    mean_val = np.mean(y_output)
-    range_val = np.max(y_output) - np.min(y_output)
-    beta = [max_val, min_val, mean_val, range_val]
-    popt, _ = curve_fit(logistic_func, y_output, y_label, p0=beta, maxfev=1000000)
-    y_output_logistic = logistic_func(y_output, *popt)
-    return y_output_logistic
 
 def main(config):
 
---
+++ VQA_PC/main/utils.py
@@ -0,0 +1,18 @@
+def logistic_func(X, bayta1, bayta2, bayta3, bayta4):
+    denominator = np.abs(bayta4) + 1e-5  # to avoid division by zero
+    numerator = np.negative(X - bayta3)
+    exponent = np.clip(np.divide(numerator, denominator), -500, 500)  # to avoid overflow and underflow
+    logisticPart = 1 + np.exp(exponent)
+    yhat = bayta2 + np.divide(bayta1 - bayta2, logisticPart)
+    return yhat
+
+
+def fit_function(y_label, y_output):
+    max_val = np.max(y_label)
+    min_val = np.min(y_label)
+    mean_val = np.mean(y_output)
+    range_val = np.max(y_output) - np.min(y_output)
+    beta = [max_val, min_val, mean_val, range_val]
+    popt, _ = curve_fit(logistic_func, y_output, y_label, p0=beta, maxfev=1000000)
+    y_output_logistic = logistic_func(y_output, *popt)
+    return y_output_logistic
